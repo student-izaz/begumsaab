4fa879552560a248f76cd3f8053373dbe7d6f21f
student-izaz <147581969+student-izaz@users.noreply.github.com>
2025-03-18 02:33:43 +0530

    Update ShowProductsByCategory.jsx

--- frontend/src/pages/ShowProductsByCategoryPage/ShowProductsByCategory.jsx
+++ frontend/src/pages/ShowProductsByCategoryPage/ShowProductsByCategory.jsx
@@ -4,15 +4,17 @@ import { fetchProductsByCategory } from '../../api/api';
 import ProductList from '../../components/ProductList/ProductList';
 import { useParams } from 'react-router-dom';
 import ShowMessage from '../../components/ShowMessage/ShowMessage';
+import { AuthContext } from '../../Store/auth';
 
 const ShowProductByCategoryPage = () => {
   const { category } = useParams();
+  const {API_URL} = useContext(AuthContext);
   const [products, setProducts] = useState([]); // Initialize as an empty array
 
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const data = await fetchProductsByCategory(category);
+        const data = await fetchProductsByCategory(category,API_URL);
         setProducts(data || []); // Ensure data is set as an array
       } catch (error) {
         console.error('Error fetching products:', error);
